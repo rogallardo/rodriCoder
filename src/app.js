dac1d1e97caca246be632083463ea51199704703
Rodrigo <rodrigogastongallardo@gmail.com>
2023-05-19 16:53:17 -0300

    optimizando codigo

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 import express from 'express';
 import { routerProducts } from './routes/routerProducts.js';
 import { routerCarts} from './routes/routerCarts.js';
-import CartManager from './controllers/CartManager.js';
-
 
 const app = express();
 const port = 8080;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-
-
-const c = new CartManager()
-c.addToCart(3,6)
 
 app.use('/api/products/', routerProducts)
 app.use('/api/carts/', routerCarts)
