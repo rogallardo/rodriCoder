4e969bf955aa12089183ffa76dc27581ff8d7400
Rodrigo <rodrigogastongallardo@gmail.com>
2023-05-20 12:21:25 -0300

    utils.js actualizado

--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -1,35 +1,27 @@
 import fs from'fs'
 import path from'path'
+import { JSONreader, JSONwriter } from './helpers/utils.js'
 
 
 
 export default class ProductManager {
-    constructor(){
-        
-        this.products = []
-        
+    constructor(){      
+        this.products = []      
         this.path =  '/src/db/products.json'
+    }
 
-    }
     async getProducts(){
-         try {
-            const file =  await fs.promises.readFile(path.resolve()  + this.path,'utf-8')
-            const fileParsed = JSON.parse(file)     
-            this.products = fileParsed
+        try {     
+            this.products = await JSONreader(this.path) 
             return this.products
-         } catch (error) {  
+        }catch (error) {  
             const productsJSON = JSON.stringify(this.products)
-            fs.writeFileSync('./src/db/products.json', productsJSON)
-         }
-        
+            fs.writeFileSync(path.resolve()  + this.path, productsJSON)
+        }       
     }
-   async getProductById(id){
-        const file =  await fs.promises.readFile(path.resolve()  + this.path,'utf-8')
-        const fileParsed = JSON.parse(file)
-        this.products = fileParsed
-       
-        let foundedProduct = this.products.find(product=> product.id === id)
-       
+    async getProductById(id){
+        this.products = await JSONreader(this.path)    
+        let foundedProduct = this.products.find(product=> product.id === id)       
         if(!foundedProduct){
             return []
         }
@@ -43,27 +35,15 @@ export default class ProductManager {
         }
         return id
     }
-    async addProduct(
-        title, // “producto prueba” 
-        description, //”Este es un producto prueba”
-        code, //abc123”,
-        price, //200,
-        status, 
-        stock, //25
-        category,//electro
-        thumbnail, //”Sin imagen”
-    ){
+    async addProduct(title, description, code, price, status, stock, category, thumbnail){
         //primero, leo el archivo y obtengo los productos
-       const file =  await fs.promises.readFile(path.resolve()  + this.path,'utf-8')
-        const fileParsed = JSON.parse(file)
-        this.products = fileParsed
-        
-        //luego realizo una validacion
+        this.products = await JSONreader(this.path)      
         //creo un objeto donde si la validacion es true, se guarda en el archivo, y si no, recibo el error
         let msg = {
             error: "",
             validation: true
         }
+        //luego realizo una validacion
         const fieldValidation = (field)=>{
             msg.error = `Error, ${field} incomplete`
             msg.validation = false
@@ -89,28 +69,16 @@ export default class ProductManager {
         code =  codeValidation(code)
         
         //LUEGO DE VALIDAR, CREAMOS EL OBJETO DEL PRODUCTO Y LO AGREGAMOS AL ARRAY
-        const newProduct = {
-        id, 
-        title, // “producto prueba” 
-        description, //”Este es un producto prueba”
-        code, //abc123”,
-        price, //200,
-        status, 
-        stock, //25
-        category,//electro
-        thumbnail}
+        const newProduct = {id, title, description, code, price, status, stock, category, thumbnail}
         if (msg.validation){
             this.products.push(newProduct)
-            const productsToJSON = JSON.stringify(this.products)
-            await fs.promises.writeFile('./src/db/products.json', productsToJSON)
+            await JSONwriter(this.path, this.products)
             return msg 
         } 
         return msg 
     } 
     async deleteById(id){
-        const file =  await fs.promises.readFile(path.resolve()  + this.path,'utf-8')
-        const fileParsed = JSON.parse(file)
-        this.products = fileParsed
+        this.products = await JSONreader(this.path)
         let foundId = this.products.some(x=> x.id === id)
         let msg = {
             deleted: null,
@@ -119,50 +87,55 @@ export default class ProductManager {
         if(foundId){
             let newArray = this.products.filter(x=> x.id!= id)
             this.products = newArray
-            const productsToJSON = JSON.stringify(this.products)
-            await fs.promises.writeFile('./src/db/products.json', productsToJSON)
+            await JSONwriter(this.path, this.products)
             msg.deleted = true
         }else{
             msg.deleted = false
             msg.error = 'cannot delete inexistent id'
         }
-        return msg
-      
-       
+        return msg       
     }
-   async updateProduct(
-        id,
-        title, // “producto prueba” 
-        description, //”Este es un producto prueba”
-        code, //abc123”,
-        price, //200,
-        status, 
-        stock, //25
-        category,//electro
-        thumbnail, //”Sin imagen”
-   ){
-    //SE LEERA EL ARCHIVO JSON Y SE PARSEARA PARA PODER TRABAJAR CON EL ARRAY
-        const file = await fs.promises.readFile(path.resolve() + this.path, 'utf-8')
-        const fileParsed = JSON.parse(file)
-        this.products = fileParsed
-        //BUSCAMOS EL ID DEL PRODUCTO A ACTUALIZAR
-        let productFound =  await this.getProductById(id)
-        
-         //creo un objeto donde si la validacion es true, se guarda en el archivo, y si no, recibo el error
+    async updateProduct(id, title, description, code, price, status, stock, category, thumbnail){
+        //lectura del json para actualizar
+        this.products = await JSONreader(this.path)
+        //buscamos el producto a actualizar pasando su id como argumento
+        let productFound =  await this.getProductById(id)        
+        //creo un objeto donde si la validacion es true, se guarda en el archivo, y si no, recibo el error
         let msg = {
             error: "",
             validation: true,
             data: {}     
         }
-        //SI LO ENCUENTRO PROCEDO CON LA VALIDACION DE LOS INPUTS
+        //si no lo encuentro, guardo en el mensaje el detalle del error
         if(productFound.length === 0){
-            console.log('me estoy ejecutando')
             msg.validation = false
             msg.error = 'product not found'  
-           
+        //si lo encuentro, actualizo el producto         
         }else{
-            productFound = {
-            
+            //validacion            
+            const fieldValidation = (field)=>{
+                msg.validation = false
+                msg.error = `Error, ${field} incomplete`
+            }
+            const codeValidation =(code)=>{
+                let newCode = this.products.find(product => product.code === code)
+                if(newCode){
+                    msg.validation = false
+                    msg.error = 'The ingresed code already exists' 
+                    return msg         
+                }else{
+                    return code
+                }
+            }      
+            status = true
+            title = title ??  fieldValidation('title')
+            price = price ??  fieldValidation('price')
+            category = category ??  fieldValidation('thumbnail')
+            status = status ??  fieldValidation('status')
+            stock = stock ??  fieldValidation('stock')
+            code =  codeValidation(code)
+            description = description ??  fieldValidation('description')      
+            productFound = {          
                 id: productFound[0].id,   
                 title: title,
                 description: description, 
@@ -171,63 +144,18 @@ export default class ProductManager {
                 status: status, 
                 stock: stock, 
                 category: category,
-                thumbnail: thumbnail}
-                //VALIDACION
-               
-                const fieldValidation = (field)=>{
-                    msg.validation = false
-                    msg.error = `Error, ${field} incomplete`
-                }
-                const codeValidation =(code)=>{
-                    let newCode = this.products.find(product => product.code === code)
-                    if(newCode){
-                        msg.validation = false
-                        msg.error = 'The ingresed code already exists' 
-                        return msg         
-                    }else{
-                        return code
-                    }
-                }      
-                status = true
-                title = title ??  fieldValidation('title')
-                price = price ??  fieldValidation('price')
-                category = category ??  fieldValidation('thumbnail')
-                status = status ??  fieldValidation('status')
-                stock = stock ??  fieldValidation('stock')
-                code =  codeValidation(code)
-                description = description ??  fieldValidation('description')
-             
-             //LUEGO DE VALIDAR, CREAMOS EL OBJETO DEL PRODUCTO Y LO AGREGAMOS AL ARRAY
+                thumbnail: thumbnail
+            }
         }
         if (msg.validation){
             await this.deleteById(id)
-             this.products = [...this.products, productFound]
-             const productsToJSON = JSON.stringify(this.products)
-             await fs.promises.writeFile('./src/db/products.json', productsToJSON)
-             msg.data = productFound
+            this.products = [...this.products, productFound]
+            await JSONwriter(this.path, this.products)
+            msg.data = productFound
          }            
         return msg          
     }
 }
-
-const producto = new ProductManager()
-
-//PARA TESTEAR, DESCOMENTAR UNA LINEA A LA VEZ, PASANDO A LA SIGUIENTE SE DEBERA COMENTAR LA ANTERIOR//
-//TENER EN CUENTA: INSTALAR EL PACKAGE.JSON DE NODE MEDIANTE NPM INIT -Y
-
-//OBTENER TODOS LOS PRODUCTOS, SE CREARA EL ARCHIVO products.json
-//producto.getProducts()
-
-//AGREGAR PRODUCTOS
-//producto.addProduct('tv', '29"', 990, 'img.jpg', 'abc14FD23', 2)
-//producto.addProduct('monitor', '14"', 990, 'im4g.jpg', 'abc1er423', 32)
-
-//ACTUALIZAR PRODUCTO
-//producto.updateProduct(1,'celular', 'Samsung', 9999, 'img.jpg', 'nd325o', 99999)
-
-//BORRAR PRODUCTO
-//producto.deleteById(1)
-
 
 
 
---
+++ src/controllers/helpers/utils.js
@@ -0,0 +1,26 @@
+import fs from 'fs'
+import path from'path'
+//funciones de lectura del JSON
+export async function JSONreader(directory){
+    try {
+        const file =  await fs.promises.readFile(path.resolve()  + directory,'utf-8')
+        const fileParsed = JSON.parse(file)
+        return fileParsed       
+    } catch (error) {
+        error('Error reading JSON')
+    }    
+}
+//funcion de escritura del JSON
+export async function JSONwriter(directory, fileToWrite){
+    try {
+        const productsJSON = JSON.stringify(fileToWrite)
+        await fs.promises.writeFile(path.resolve()  + directory, productsJSON)
+        return    
+    } catch (error) {
+        error('Error writing JSON')
+    }    
+}
+
+
+
+
